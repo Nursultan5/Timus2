059cd85105871b1052ae5346a4b58b22d82efad8
Нурсултан Таргынов <ntargynov@edu.hse.ru>
2018-02-21 23:47:09 +0300

    1349

---
+++ 1349/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1349
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n == 1) Console.WriteLine(1);
+            else if (n == 2) Console.WriteLine(3 + " " + 4 + " " + 5);
+            else Console.WriteLine(-1);
+      
+        }
+    }
+}
